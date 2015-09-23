a0af4a09af794a33a76e1a28d46aaabbe1948314
yuezianmian <540388758@qq.com>
2015-09-23 10:28:25 +0800

    edit1

--- data/model/banner.model.php
+++ data/model/banner.model.php
@@ -1,22 +1,23 @@
+
 <?php
 /**
- * 产品模型
+ * banner模型
  *
  */
 defined('InShopNC') or exit('Access Invalid!');
-class productModel extends Model {
+class bannerModel extends Model {
 
 	public function __construct(){
-		parent::__construct('product');
+		parent::__construct('banner');
 	}
 
 	/**
-	 * 取产品列表
+	 * 取banner列表
 	 * @param unknown $condition
 	 * @param string $pagesize
 	 * @param string $order
 	 */
-	public function getProductList($condition = array(),$field = '*', $pagesize = '', $limit = '', $order = 'product_id asc') {
+	public function getBannerList($condition = array(),$field = '*', $pagesize = '', $limit = '', $order = 'banner_order asc') {
 		return $this->field($field)->where($condition)->page($pagesize)->order($order)->limit($limit)->select();
 	}
 
@@ -24,33 +25,33 @@ class productModel extends Model {
 	 * 取得单条信息
 	 * @param unknown $condition
 	 */
-	public function getProductInfo($condition = array()) {
+	public function getBannerInfo($condition = array()) {
 		return $this->where($condition)->find();
 	}
 
 	/**
-	 * 删除产品
+	 * 删除banner
 	 * @param unknown $condition
 	 */
-	public function delProduct($condition = array()) {
+	public function delBanner($condition = array()) {
 		return $this->where($condition)->delete();
 	}
 
 	/**
-	 * 增加产品
+	 * 增加banner
 	 * @param unknown $data
 	 * @return boolean
 	 */
-	public function addProduct($data) {
+	public function addBanner($data) {
 		return $this->insert($data);
 	}
 
 	/**
-	 * 更新产品
+	 * 更新banner
 	 * @param unknown $data
 	 * @param unknown $condition
 	 */
-	public function editProduct($data = array(),$condition = array()) {
+	public function editBanner($data = array(),$condition = array()) {
 		return $this->where($condition)->update($data);
 	}
 }
